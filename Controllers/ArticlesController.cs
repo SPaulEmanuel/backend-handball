ef3eac0e5cddb67f0d3b8b8a3f0686ed1db33e57
Gabriel mocanu <gabriel.mocanu@student.usv.ro>
2023-10-18 20:06:06 +0300

    Add two new files

---
+++ Controllers/ArticlesController.cs
@@ -0,0 +1,81 @@
+﻿using aplicatieHandbal.Data;
+using aplicatieHandbal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace aplicatieHandbal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticoleController : Controller
+    {
+        private readonly AplicatieDBContext _aplicatieDBContext;
+
+        public ArticoleController(AplicatieDBContext aplicatieDBContext)
+        {
+            _aplicatieDBContext = aplicatieDBContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllArticole()
+        {
+            var articole = await _aplicatieDBContext.Articole.ToListAsync();
+            return Ok(articole);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddArticole([FromBody] Articole articoleRequest)
+        {
+            articoleRequest.Id = Guid.NewGuid();
+            await _aplicatieDBContext.Articole.AddAsync(articoleRequest);
+            await _aplicatieDBContext.SaveChangesAsync();
+            return Ok(articoleRequest);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetArticole([FromRoute] Guid id)
+        {
+            var articole = await _aplicatieDBContext.Articole.FirstOrDefaultAsync(x => x.Id == id);
+            if (articole == null)
+            {
+                return NotFound();
+            }
+            return Ok(articole);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateArticole([FromRoute] Guid id, Articole updateArticoleReq)
+        {
+            var articole = await _aplicatieDBContext.Articole.FindAsync(id);
+            if (articole == null)
+            {
+                return NotFound();
+            }
+
+            articole.Title = updateArticoleReq.Title;
+            articole.Author = updateArticoleReq.Author;
+            articole.Content = updateArticoleReq.Content;
+            articole.DatePublished = updateArticoleReq.DatePublished;
+            articole.ImageUrl = updateArticoleReq.ImageUrl;
+
+            await _aplicatieDBContext.SaveChangesAsync();
+            return Ok(articole);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteArticole([FromRoute] Guid id)
+        {
+            var articole = await _aplicatieDBContext.Articole.FindAsync(id);
+            if (articole == null)
+            {
+                return NotFound();
+            }
+            _aplicatieDBContext.Articole.Remove(articole);
+            await _aplicatieDBContext.SaveChangesAsync();
+            return Ok(articole);
+        }
+    }
+}
---
+++ Controllers/GamesController.cs
@@ -0,0 +1,82 @@
+﻿using aplicatieHandbal.Data;
+using aplicatieHandbal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace aplicatieHandbal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : Controller
+    {
+        private readonly AplicatieDBContext _aplicatieDBContext;
+        public GamesController(AplicatieDBContext aplicatieDBContext)
+        {
+            _aplicatieDBContext = aplicatieDBContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var games = await _aplicatieDBContext.Games.ToListAsync();
+            return Ok(games);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddGame([FromBody] Game gameRequest)
+        {
+            gameRequest.Id = Guid.NewGuid();
+            await _aplicatieDBContext.Games.AddAsync(gameRequest);
+            await _aplicatieDBContext.SaveChangesAsync();
+            return Ok(gameRequest);
+        }
+        [HttpGet]
+
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetGame([FromRoute] Guid id)
+        {
+            var game = await _aplicatieDBContext.Games.FirstOrDefaultAsync(x => x.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return Ok(game);
+        }
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateGame([FromRoute] Guid id, Game updateGameReq)
+        {
+            var game = await _aplicatieDBContext.Games.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            game.Title = updateGameReq.Title;
+            game.Date = updateGameReq.Date;
+            game.Location = updateGameReq.Location;
+            game.Result = updateGameReq.Result;
+            game.Referee = updateGameReq.Referee;
+            game.Status = updateGameReq.Status;
+            game.Description = updateGameReq.Description;
+            game.Attendance = updateGameReq.Attendance;
+            game.MediaUrl = updateGameReq.MediaUrl;
+
+            await _aplicatieDBContext.SaveChangesAsync();
+            return Ok(game);
+        }
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> deleteGame([FromRoute] Guid id)
+        {
+            var game = await _aplicatieDBContext.Games.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            _aplicatieDBContext.Games.Remove(game);
+            await _aplicatieDBContext.SaveChangesAsync();
+            return Ok(game);
+        }
+
+    }
+}
