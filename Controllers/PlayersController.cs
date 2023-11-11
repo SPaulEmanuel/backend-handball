37fa875b500018a8a82e02e5f3c1157beef2d0ed
gabriM <gabriel.mocanu@student.usv.ro>
2023-11-11 18:44:22 +0200

    DtoPlayer

--- Controllers/PlayersController.cs
+++ Controllers/PlayersController.cs
@@ -1,4 +1,3 @@
-
 ﻿using aplicatieHandbal.Data;
 using aplicatieHandbal.Models;
 using aplicatieHandbal.Services;
@@ -24,6 +23,11 @@ namespace aplicatieHandbal.Controllers
         {
   
             return Ok(await _playerService.GetAllPlayers());
+        }
+        [HttpGet("GetAllInfo")]
+        public async Task<IActionResult> GetAllInfoPlayers()
+        {
+            return Ok(await _playerService.GetAllInfoPlayers());
         }
         [HttpPost]
         public async Task<IActionResult> AddPlayer([FromBody] Player playerRequest)
@@ -53,59 +57,3 @@ namespace aplicatieHandbal.Controllers
         }
     }
 }
-
-﻿using aplicatieHandbal.Data;
-using aplicatieHandbal.Models;
-using aplicatieHandbal.Services;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.JsonPatch;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
-namespace aplicatieHandbal.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PlayerController : Controller
-    {
-        private readonly IPlayerService _playerService;
-
-        public PlayerController(IPlayerService playerService)
-        {
-            _playerService = playerService!; 
-        }
-        [HttpGet("GetAll")]
-        public async Task<IActionResult> GetAllPlayers()
-        {
-  
-            return Ok(await _playerService.GetAllPlayers());
-        }
-        [HttpPost]
-        public async Task<IActionResult> AddPlayer([FromBody] Player playerRequest)
-        {
-            
-            return Ok(await _playerService.AddPlayer(playerRequest));
-        }
-        [HttpGet]
-
-        [Route("{id:Guid}")]
-        public async Task<IActionResult> GetPlayer([FromRoute] Guid id)
-        {   
-            return Ok(await _playerService.GetPlayerById(id));
-        }
-       
-        [HttpPut]
-        [Route("{id:Guid}")]
-        public async Task<IActionResult> updatePlayer([FromRoute] Guid id, Player updatePlayerReq)
-        {
-            return Ok(await _playerService.UpdatePlayer(id, updatePlayerReq));
-        }
-        [HttpDelete]
-        [Route("{id:Guid}")]
-        public async Task<IActionResult> deletePlayer([FromRoute] Guid id)
-        {
-            return Ok(await _playerService.DeletePlayer(id));
-        }
-    }
-}
-
--- Services/PlayerService.cs
+++ Services/PlayerService.cs
@@ -1,4 +1,3 @@
-
 ﻿using aplicatieHandbal.Data;
 using aplicatieHandbal.Models;
 using aplicatieHandbal.Validators;
@@ -9,7 +8,8 @@ namespace aplicatieHandbal.Services
 {
     public interface IPlayerService
     {
-        Task<List<Player>> GetAllPlayers();
+        Task<List<PlayerDto>> GetAllPlayers();
+        Task<List<Player>> GetAllInfoPlayers();
         Task<Player> AddPlayer(Player player);
         Task<Player> GetPlayerById(Guid id);
         Task<Player> UpdatePlayer(Guid id, Player updatedPlayer);
@@ -45,11 +45,26 @@ namespace aplicatieHandbal.Services
             
         }
 
-        public async Task<List<Player>> GetAllPlayers()
+        public async Task<List<Player>> GetAllInfoPlayers()
         {
             var players = await _aplicatieDBContext.Players.ToListAsync();
             return players;
         }
+
+        public async Task<List<PlayerDto>> GetAllPlayers()
+        {
+            var players = await _aplicatieDBContext.Players
+                .Select(player => new PlayerDto
+                {
+                    Name = player.Name,
+                    Vorname = player.Vorname,
+                    ImageUrl = player.ImageUrl
+                })
+                .ToListAsync();
+
+            return players;
+        }
+
 
         public async Task<Player> GetPlayerById(Guid id)
         {
@@ -93,95 +108,8 @@ namespace aplicatieHandbal.Services
             }
             throw new Exception("ID not found");
         }
+
+
     }
 
 }
-
-﻿using aplicatieHandbal.Data;
-using aplicatieHandbal.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace aplicatieHandbal.Services
-{
-    public interface IPlayerService
-    {
-        Task<List<Player>> GetAllPlayers();
-        Task<Player> AddPlayer(Player player);
-        Task<Player> GetPlayerById(Guid id);
-        Task<Player> UpdatePlayer(Guid id, Player updatedPlayer);
-        Task<Player> DeletePlayer(Guid id);
-    }
-
-    public class PlayerService : IPlayerService
-    {
-        private readonly AplicatieDBContext _aplicatieDBContext;
-        public PlayerService(AplicatieDBContext dbContext)
-        {
-            _aplicatieDBContext = dbContext;
-        }
-        public async Task<Player> AddPlayer(Player playerRequest)
-        {
-            playerRequest.PlayerID = Guid.NewGuid();
-            await _aplicatieDBContext.Players.AddAsync(playerRequest);
-            await _aplicatieDBContext.SaveChangesAsync();
-            return playerRequest;
-        }
-
-        public async Task<Player> DeletePlayer(Guid id)
-        {
-            var player = await _aplicatieDBContext.Players.FindAsync(id);
-           if (player is not null) {
-                _aplicatieDBContext.Players.Remove(player);
-                await _aplicatieDBContext.SaveChangesAsync();
-                return player;
-            }
-           throw new Exception("Cannot delete id");
-            
-        }
-
-        public async Task<List<Player>> GetAllPlayers()
-        {
-            var players = await _aplicatieDBContext.Players.ToListAsync();
-            return players;
-        }
-
-        public async Task<Player> GetPlayerById(Guid id)
-        {
-            var player = await _aplicatieDBContext.Players.FirstOrDefaultAsync(x => x.PlayerID == id);
-            if (player is not null)
-            {
-                return player;
-                
-            }
-            throw new Exception("Player not found");
-        }
-
-        public async Task<Player> UpdatePlayer(Guid id, Player updatePlayerReq)
-        {
-            var player = await _aplicatieDBContext.Players.FindAsync(id);
-            if (player is not null)
-            {
-                player.Name = updatePlayerReq.Name;
-                player.Vorname = updatePlayerReq.Vorname;
-                player.Age = updatePlayerReq.Age;
-                player.Position = updatePlayerReq.Position;
-                player.Height = updatePlayerReq.Height;
-                player.Weight = updatePlayerReq.Weight;
-                player.Nationality = updatePlayerReq.Nationality;
-                player.JerseyNumber = updatePlayerReq.JerseyNumber;
-                player.ContractStartDate = updatePlayerReq.ContractStartDate;
-                player.ContractEndDate = updatePlayerReq.ContractEndDate;
-                player.Salary = updatePlayerReq.Salary;
-                player.GoalsScored = updatePlayerReq.GoalsScored;
-                player.ImageUrl = updatePlayerReq.ImageUrl;
-                player.InstagramProfile = updatePlayerReq.InstagramProfile;
-
-                await _aplicatieDBContext.SaveChangesAsync();
-                return player;
-            }
-            throw new Exception("ID not found");
-        }
-    }
-
-}
-
