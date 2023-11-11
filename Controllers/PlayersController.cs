57b141c6c0bd0e120b7c2ffca5d257eeca4f4178
gabriM <gabriel.mocanu@student.usv.ro>
2023-11-11 11:04:50 +0200

    newControler

--- Controllers/PlayersController.cs
+++ Controllers/PlayersController.cs
@@ -14,99 +14,59 @@ namespace aplicatieHandbal.Controllers
     {
         private readonly IPlayerService _playerService;
 
-      
-        private readonly AplicatieDBContext _aplicatieDBContext;
-        public PlayerController(IPlayerService playerService, AplicatieDBContext dbContext)
+        public PlayerController(IPlayerService playerService)
         {
-            _playerService = playerService!;
-            _aplicatieDBContext = dbContext;
+            _playerService = playerService!; 
         }
-        [HttpGet]
+        [HttpGet("GetAll")]
         public async Task<IActionResult> GetAllPlayers()
         {
-            var players = await _aplicatieDBContext.Players.ToListAsync();
+
+            var players = await _playerService.GetAllPlayers();
             return Ok(players);
         }
         [HttpPost]
         public async Task<IActionResult> AddPlayer([FromBody] Player playerRequest)
         {
-            playerRequest.PlayerID = Guid.NewGuid();
-            await _aplicatieDBContext.Players.AddAsync(playerRequest);
-            await _aplicatieDBContext.SaveChangesAsync();
-            return Ok(playerRequest);
+            
+            var player=await _playerService.AddPlayer(playerRequest);
+            return Ok(player);
         }
         [HttpGet]
 
         [Route("{id:Guid}")]
         public async Task<IActionResult> GetPlayer([FromRoute] Guid id)
         {
-            var player = await _aplicatieDBContext.Players.FirstOrDefaultAsync(x => x.PlayerID == id);
+            var player = await _playerService.GetPlayerById(id);
             if (player == null)
             {
                 return NotFound();
             }
             return Ok(player);
         }
-        [HttpPatch]
-        [Route("{id:Guid}")]
-        public async Task<IActionResult> PatchPlayer([FromRoute] Guid id, [FromBody] JsonPatchDocument<Player> patchDocument)
-        {
-            if (patchDocument == null)
-            {
-                return BadRequest();
-            }
-
-            var player = await _aplicatieDBContext.Players.FindAsync(id);
-            if (player == null)
-            {
-                return NotFound();
-            }
-
-            patchDocument.ApplyTo(player);
-
-            await _aplicatieDBContext.SaveChangesAsync();
-            return Ok(player);
-        }
-
+       
         [HttpPut]
         [Route("{id:Guid}")]
         public async Task<IActionResult> updatePlayer([FromRoute] Guid id, Player updatePlayerReq)
         {
-            var player = await _aplicatieDBContext.Players.FindAsync(id);
+            var player = await _playerService.UpdatePlayer(id, updatePlayerReq);
             if (player == null)
             {
                 return NotFound();
             }
 
-            player.Name = updatePlayerReq.Name;
-            player.Vorname = updatePlayerReq.Vorname;
-            player.Age = updatePlayerReq.Age;
-            player.Position = updatePlayerReq.Position;
-            player.Height = updatePlayerReq.Height;
-            player.Weight = updatePlayerReq.Weight;
-            player.Nationality = updatePlayerReq.Nationality;
-            player.JerseyNumber = updatePlayerReq.JerseyNumber;
-            player.ContractStartDate = updatePlayerReq.ContractStartDate;
-            player.ContractEndDate = updatePlayerReq.ContractEndDate;
-            player.Salary = updatePlayerReq.Salary;
-            player.GoalsScored = updatePlayerReq.GoalsScored;
-            player.ImageUrl = updatePlayerReq.ImageUrl;
-            player.InstagramProfile = updatePlayerReq.InstagramProfile;
-
-            await _aplicatieDBContext.SaveChangesAsync();
             return Ok(player);
         }
         [HttpDelete]
         [Route("{id:Guid}")]
         public async Task<IActionResult> deletePlayer([FromRoute] Guid id)
         {
-            var player = await _aplicatieDBContext.Players.FindAsync(id);
+            var player = await _playerService.DeletePlayer(id);
             if (player == null)
             {
                 return NotFound();
             }
-            _aplicatieDBContext.Players.Remove(player);
-            await _aplicatieDBContext.SaveChangesAsync();
+
             return Ok(player);
         }
     }
