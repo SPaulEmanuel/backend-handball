4a299c5c9dc8a27b3cb203a3bafcbf50f61f9b78
Gabri1989 <106927775+Gabri1989@users.noreply.github.com>
2023-12-05 21:45:00 +0200

    Update Staff.cs

--- Models/Staff.cs
+++ Models/Staff.cs
@@ -4,7 +4,7 @@
     {
         public Guid StaffID { get; set; }
         public string Name { get; set; }
-        public string Vorname { get; set; }
+        public string Surname { get; set; }
         public string ImageUrl { get; set; }
     }
 }
